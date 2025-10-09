64d89c0f8394dc4879f4617d2114fa5ccaf77f28
rh925 <ruixuan.han25@imperial.ac.uk>
2025-10-09 10:41:32 +0100

    updated python code

---
+++ week2/code/loops.py
@@ -0,0 +1,26 @@
+# FOR looops
+for i in range(5):
+    print(i)
+
+my_list = [0, 2, "geronimo!", 3.0, True, False]
+for k in my_list:
+    print(k)
+
+total = 0
+summands = [0, 1, 11, 111, 1111]
+for s in summands:
+    total = total + s
+    print(total)
+
+# # WHITE loop
+# z = 0
+# while z < 100:
+#     z = z + 1
+#     print(z)
+
+
+x = 0
+test = [0, 1, 2, 3, 4, 5]
+for y in test:
+    x = x + y
+    print(x)
