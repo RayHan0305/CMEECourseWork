e9535ff71d4b07a6b579bee29b4c2e6a98295ba3
rh925 <ruixuan.han25@imperial.ac.uk>
2025-10-19 18:18:22 +0100

    updated

--- week1/code/tiff2png.sh
+++ week1/code/tiff2png.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Check if the files are provided.
-# compgen -G "*.tif" is used to checks if any .tif files exist
-if compgen -G "*.tif" > /dev/null; then
+if ls *.tif; then
     for f in *.tif; 
         do  
             echo "Converting $f"; 
