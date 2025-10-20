20e7211b0ee1c1c435a5b3ef262eac0f58eeae82
rh925 <ruixuan.han25@imperial.ac.uk>
2025-10-20 15:45:01 +0100

    updated

--- week2/code/test.py
+++ week2/code/test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sc
 
 a = np.array(range(5), float)
 
@@ -31,5 +32,38 @@ print(mat[1,0])
 print(mat[:,0])
 # 1st row, 2nd column element
 print(mat[0,1])
-#
+# 1st row, 2nd column element
 print(mat[0,-1])
+# 1st row, 1st column element
+print(mat[0,-2])
+# 2nd row, 1st column element
+print(mat[-1,0])
+# replace a single element
+mat[0,0] = -1
+print(mat)
+# replace whole column
+mat[:,0] = [12,12] 
+print(mat)
+# append row, note axis specification
+np.append(mat, [[12,12]], axis = 0)
+print(mat)
+# reshape
+print(mat.reshape((1,4)))
+print(np.ones((4,2)))
+# Initialize a (4,2) matrix with zeros
+print(np.zeros((4,2)))
+# create an identity matrix
+m = np.identity(4)
+print(m)
+# fill the matrix with 16
+m = m.fill(16)
+print(m)
+mm = np.arange(16)
+mm = mm.reshape(4,4) # COnvert to matrix
+print(mm)
+# 10 samples from the normal distribution
+print(sc.stats.norm.rvs(size = 10))
+print(sc.stats.norm.rvs(size = 5, random_state = 1234))
+# example of generating random integers between 0 and 10
+print(sc.stats.randint.rvs(0, 10, size = 7))
+print(sc.stats.randint.rvs(0, 10, size = 7, random_state = 1234))
